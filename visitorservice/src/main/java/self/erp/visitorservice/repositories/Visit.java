403de0cfa22608413c30ae92c0a484951f2d0eb7
BilalAM <bilal.asif.97@gmail.com>
2019-03-10 21:35:41 +0500

    adding further packages for crud

---
+++ visitorservice/src/main/java/self/erp/visitorservice/repositories/Visit.java
@@ -0,0 +1,8 @@
+package self.erp.visitorservice.repositories;
+
+import javax.persistence.Table;
+
+@Table(name = "Schema_Visitor")
+public class Visit {
+
+}
---
+++ visitorservice/src/main/java/self/erp/visitorservice/repositories/VisitRepository.java
@@ -0,0 +1,6 @@
+package self.erp.visitorservice.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VisitRepository extends JpaRepository<> {
+}

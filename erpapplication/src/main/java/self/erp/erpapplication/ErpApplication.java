6b6e08697664d8c875235b47a553cf808d073bf1
BilalAM <bilal.asif.97@gmail.com>
2019-03-16 14:19:56 +0500

    annotations for the main app

--- erpapplication/src/main/java/self/erp/erpapplication/ErpApplication.java
+++ erpapplication/src/main/java/self/erp/erpapplication/ErpApplication.java
@@ -2,10 +2,18 @@ package self.erp.erpapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-import self.erp.visitorservice.controller.VisitController;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@ComponentScan(basePackageClasses = { VisitController.class })
+/**
+ * This is the main spring boot application of erp.
+ * It scans all the controllers , models and repositories for the spring application context
+ * Just run this as a simple jar
+ */
+@ComponentScan(basePackages = "self.erp.visitorservice")
+@EnableJpaRepositories(basePackages = { "self.erp.visitorservice.repositories" })
+@EntityScan(basePackages = { "self.erp.visitorservice.repositories" })
 @SpringBootApplication
 public class ErpApplication {
     public static void main(String[] args) {

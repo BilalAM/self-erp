08d50cdb169057531a784725a5a6d6abbdb97c10
BilalAM <bilal.asif.97@gmail.com>
2019-03-16 13:39:38 +0500

    added controller

---
+++ visitorservice/src/main/java/self/erp/visitorservice/VisitorServiceUtils.java
@@ -0,0 +1,11 @@
+package self.erp.visitorservice;
+
+import org.apache.commons.lang3.StringUtils;
+import self.erp.visitorservice.repositories.Visit;
+
+public class VisitorServiceUtils {
+    private static boolean validateVisit(Visit visit) {
+        return true;
+        // if(StringUtils.isE)
+    }
+}
--- visitorservice/src/main/java/self/erp/visitorservice/controller/VisitController.java
+++ visitorservice/src/main/java/self/erp/visitorservice/controller/VisitController.java
@@ -1,4 +1,30 @@
 package self.erp.visitorservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import self.erp.visitorservice.repositories.Visit;
+import self.erp.visitorservice.repositories.VisitRepository;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@RestController(value = "/erp/visits")
 public class VisitController {
+
+    private static final Logger LOGGER = Logger.getLogger("VisitController");
+
+    @Autowired
+    private VisitRepository visitRepository;
+
+    @RequestMapping(value = "/new", method = RequestMethod.POST)
+    public ResponseEntity<String> insert(@RequestBody Visit visit) {
+        LOGGER.log(Level.INFO, "Adding [ " + visit.toString() + " ]");
+        visitRepository.save(visit);
+        return new ResponseEntity<>("Added", HttpStatus.OK);
+    }
 }

ff055bb8608f1df015d2e631fac9570714818a3c
BilalAM <bilal.asif.97@gmail.com>
2019-04-06 20:29:28 +0500

    added small logging in main method

--- ui/src/main/java/self/erp/ui/Main.java
+++ ui/src/main/java/self/erp/ui/Main.java
@@ -5,12 +5,19 @@ import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Main extends Application {
+
+    private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
 
     @Override
     public void start(Stage stage) throws Exception {
         stage.setScene(new Scene(new Pane(), 800, 600));
         stage.show();
+        LOGGER.log(Level.INFO, "Java Version [ " + System.getProperty("java.version") + " ] with JavaFx version [ "
+                + System.getProperty("javafx.version") + " ]");
     }
 
     public static void main(String[] args) {

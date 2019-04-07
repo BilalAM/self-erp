6d9382d50fc70744adfd92b817e22bfa209c2919
BilalAM <bilal.asif.97@gmail.com>
2019-04-07 22:50:55 +0500

    added main UI skeleton along with fxml file in UI

--- ui/src/main/java/self/erp/ui/Main.java
+++ ui/src/main/java/self/erp/ui/Main.java
@@ -1,20 +1,24 @@
 package self.erp.ui;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Main extends Application {
-
     private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
 
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setScene(new Scene(new Pane(), 800, 600));
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/fxmls/self-erp.fxml"));
+
+        Parent root = loader.load();
+        stage.setScene(new Scene(root));
         stage.show();
         LOGGER.log(Level.INFO, "Java Version [ " + System.getProperty("java.version") + " ] with JavaFx version [ "
                 + System.getProperty("javafx.version") + " ]");

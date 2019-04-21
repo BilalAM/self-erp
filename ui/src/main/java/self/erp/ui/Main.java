044dca1dca453f3ed70a060e1308bc9e439b0b79
BilalAM <bilal.asif.97@gmail.com>
2019-04-22 00:33:33 +0500

    Configured JavaFX with Spring-boot :)

--- ui/src/main/java/self/erp/ui/Main.java
+++ ui/src/main/java/self/erp/ui/Main.java
@@ -5,26 +5,51 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+@SpringBootApplication
+@ComponentScan(value = { "self.erp.commons.restful", "self.erp.ui.controllers" })
 public class Main extends Application {
     private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
 
-    @Override
-    public void start(Stage stage) throws Exception {
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(getClass().getResource("/fxmls/self-erp.fxml"));
+    private ConfigurableApplicationContext configurableApplicationContext;
+    private Parent root;
 
-        Parent root = loader.load();
-        stage.setScene(new Scene(root));
-        stage.show();
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
         LOGGER.log(Level.INFO, "Java Version [ " + System.getProperty("java.version") + " ] with JavaFx version [ "
                 + System.getProperty("javafx.version") + " ]");
     }
 
-    public static void main(String[] args) {
-        launch(args);
+    @Override
+    public void init() throws Exception {
+        configurableApplicationContext = SpringApplication.run(Main.class);
+        FXMLLoader loader = new FXMLLoader();
+
+        loader.setLocation(getClass().getResource("/fxmls/visits.fxml"));
+        loader.setControllerFactory(configurableApplicationContext::getBean);
+        root = loader.load();
+        for (String beanDefName : configurableApplicationContext.getBeanDefinitionNames()) {
+            LOGGER.log(Level.SEVERE, beanDefName);
+        }
+
+    }
+
+    @Override
+    public void stop() throws Exception {
+        configurableApplicationContext.close();
     }
 }

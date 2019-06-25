3260e8b8dad8addc8c9ae9b18ea438eb2fff617a
BilalAM <bilal.asif.97@gmail.com>
2019-06-25 22:47:35 +0500

    Updated ErpApplication to include PreReqs class.

--- erpapplication/src/main/java/self/erp/erpapplication/ErpApplication.java
+++ erpapplication/src/main/java/self/erp/erpapplication/ErpApplication.java
@@ -1,9 +1,9 @@
 package self.erp.erpapplication;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import self.erp.commons.prereqs.PreReqs;
 
 /**
  * This is the main spring boot application of erp. It scans all the controllers , models and repositories for the
@@ -17,6 +17,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 )
 public class ErpApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ErpApplication.class, args);
+        PreReqs.startMysqlService();
+        // SpringApplication.run(ErpApplication.class, args);
     }
 }

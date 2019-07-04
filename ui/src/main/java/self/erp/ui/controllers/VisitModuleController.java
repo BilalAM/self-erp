ed87aa3bdda71fe761d30c66951b55b1bce4c0a0
BilalAM <bilal.asif.97@gmail.com>
2019-07-05 00:40:52 +0500

    Transition effect not working .

--- ui/src/main/java/self/erp/ui/controllers/VisitModuleController.java
+++ ui/src/main/java/self/erp/ui/controllers/VisitModuleController.java
@@ -17,37 +17,23 @@ import self.erp.visitorservice.repositories.Visit;
 import javax.ws.rs.core.Response;
 import java.util.logging.Logger;
 
-@Component
-public class VisitModuleController {
-    private static final Logger LOGGER = Logger.getLogger("VisitModuleController");
+@Component public class VisitModuleController {
+        private static final Logger LOGGER = Logger.getLogger("VisitModuleController");
 
-    @Autowired
-    private RestfulHelper restfulHelper;
+        @Autowired private RestfulHelper restfulHelper;
 
-    @FXML
-    private TextField visitorNameField;
-    @FXML
-    private TextField visitorVisitPurposeField;
-    @FXML
-    private TextArea visitorVisitPurposeDescriptionField;
-    @FXML
-    private LocalDateTimeTextField fromDateField;
-    @FXML
-    private LocalDateTimeTextField toDateField;
-    @FXML
-    private RadioButton completedFlag;
-    @FXML
-    private RadioButton inProgressFlag;
-    @FXML
-    private Button saveBtn;
+        @FXML private TextField visitorNameField;
+        @FXML private TextField visitorVisitPurposeField;
+        @FXML private TextArea visitorVisitPurposeDescriptionField;
+        @FXML private LocalDateTimeTextField fromDateField;
+        @FXML private LocalDateTimeTextField toDateField;
+        @FXML private RadioButton completedFlag;
+        @FXML private RadioButton inProgressFlag;
+        @FXML private Button saveBtn;
 
         @FXML private Label scsMsgLabel;
 
         @FXML private Label errorMsgLabel;
-
-
-
-
 
         @FXML private TableView<Visit> visitGrid;
 
@@ -63,21 +49,20 @@ public class VisitModuleController {
                 visitGrid.getColumns().get(4).setCellValueFactory(new PropertyValueFactory<>("visitPurpose"));
         }
 
-    @FXML private void getVisitsAction() {
-            Visit[] visitors = (Visit[]) restfulHelper.get("http://localhost:8880/erp/visit/visitors", Visit[].class);
-            for (Visit visit : visitors) {
-                    visitorNameField.appendText(visit.getVisitorName());
-                    visitorVisitPurposeField.appendText(visit.getVisitPurpose());
-                    visitorVisitPurposeDescriptionField.appendText(visit.getVisitPurposeDescription());
-                    fromDateField.setText(visit.getFromDate().toString());
-                    toDateField.setText(visit.getEndDate().toString());
-            }
-            scsMsgLabel.setVisible(true);
-            FadeTransition transitionEffect = ComponentUtils.createTransition(scsMsgLabel, 1.6, false);
-            transitionEffect.play();
-    }
+        @FXML private void getVisitsAction() {
+                Visit[] visitors = (Visit[]) restfulHelper
+                        .get("http://localhost:8880/erp/visit/visitors", Visit[].class);
+                for (Visit visit : visitors) {
+                        visitorNameField.appendText(visit.getVisitorName());
+                        visitorVisitPurposeField.appendText(visit.getVisitPurpose());
+                        visitorVisitPurposeDescriptionField.appendText(visit.getVisitPurposeDescription());
+                        fromDateField.setText(visit.getFromDate().toString());
+                        toDateField.setText(visit.getEndDate().toString());
+                }
+        }
 
         @FXML private void addVisitAction() {
+                FadeTransition fadeTransition = null;
                 Visit visit = new Visit();
                 visit.setVisitorName(visitorNameField.getText());
                 visit.setVisitPurpose(visitorVisitPurposeField.getText());
@@ -91,10 +76,15 @@ public class VisitModuleController {
                 }
                 Response response = restfulHelper.post("http://localhost:8880/erp/visit/new", visit);
                 if (!(response.getStatus() == HttpStatus.OK.value())) {
-
+                        scsMsgLabel.setVisible(true);
+                        fadeTransition = ComponentUtils.createTransition(scsMsgLabel, 1.6, false);
+                        fadeTransition.play();
+                        scsMsgLabel.setVisible(false);
                 } else {
                         errorMsgLabel.setVisible(true);
-                        // display a green label , record [ record.toString() ] added
+                        fadeTransition = ComponentUtils.createTransition(errorMsgLabel, 1.6, false);
+                        fadeTransition.play();
+                        errorMsgLabel.setVisible(false);
                 }
         }
 }

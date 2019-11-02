8f3960ba5e2685350e7dc00163db70448465a295
BilalAM <bilal.asif.97@gmail.com>
2019-11-02 18:57:38 +0500

    Created a shell executing class to reduce code mess .

--- commons/src/main/java/self/erp/commons/prereqs/DatabasePreRequisites.java
+++ commons/src/main/java/self/erp/commons/prereqs/DatabasePreRequisites.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
 import self.erp.commons.restful.RestfulHelper;
+import self.erp.commons.toolbox.ScriptTools;
 import self.erp.commons.toolbox.StringTools;
 
 import javax.annotation.PostConstruct;
@@ -29,6 +30,9 @@ public class DatabasePreRequisites implements ServletContextListener {
     @Value("${bash.root.password}")
     private String sudoPassword;
 
+    @Value("${mysql.password}")
+    private String mysqlPassword;
+
     @Autowired
     private ResourceLoader resourceLoader;
 
@@ -36,38 +40,23 @@ public class DatabasePreRequisites implements ServletContextListener {
     private RestfulHelper restfulHelper;
 
     private static final Logger LOGGER = Logger.getLogger(DatabasePreRequisites.class.getClass());
-    private static String START_MYSQL_SERVICE = "echo \"" + "%s" + "\"| sudo service mysql start ";
-    private static final String BASH_PREFIX = "/bin/bash";
-    private static final String C_FLAG = "-c";
+    private static String START_MYSQL_SERVICE = "echo '" + "%s" + "' | sudo -S service mysql start ";
     private static final String WHICH_MYSQL = "which mysql";
-    private static final String MYSQL_DOWNLOAD_LINK = "https://dev.mysql.com/get/Downloads/MySQL-5.7/mysql-5.7.27-linux-glibc2.12-i686.tar.gz";
-    private static final String MYSQL_BINARY_PATH = "/tmp/mysql.tar.gz";
 
     /**
      * Starts the MySQL service AS ROOT USER before the Hikari beans are instantiated .
      */
     @PostConstruct
     public void startMysqlService() {
-        installMYSQL();
         LOGGER.info("Starting up mysql service on host as sudo user [ " + sudoPassword + " ]");
-        int returnCode = -100;
         try {
-            LOGGER.info(START_MYSQL_SERVICE);
-            String[] cmd = { BASH_PREFIX, C_FLAG, String.format(START_MYSQL_SERVICE, sudoPassword) };
-            Process p = Runtime.getRuntime().exec(cmd);
-            returnCode = p.waitFor();
-            if (returnCode == 0) {
-                String message = "Successfully started mysql service as sudo user";
-                LOGGER.info(message);
-                System.out.println(IOUtils.toString(p.getInputStream()));
+            if (mySQLExists()) {
+                ScriptTools.execute(String.format(START_MYSQL_SERVICE, sudoPassword));
             } else {
-                String message = "Error ! , Unable to start mysql service as sudo user";
-                LOGGER.error(message);
-                throw new RuntimeException(message);
+                installMYSQL();
             }
         } catch (Exception e) {
-            LOGGER.error("Something went wrong", e);
-            throw new RuntimeException("Halting execution of program , logs : ", e);
+
         }
     }
 
@@ -77,18 +66,12 @@ public class DatabasePreRequisites implements ServletContextListener {
      * @return : true if which mysql returns the path of the installed binary . false if it returns nothing , which
      *         means mysql is not installed .
      */
-    private static boolean doesMySQLExists() {
-        Process whichCommandProcess;
-        int statusCode = 100;
+    private static boolean mySQLExists() {
         try {
-            String[] whichCommand = new String[] { BASH_PREFIX, C_FLAG, WHICH_MYSQL };
-            whichCommandProcess = Runtime.getRuntime().exec(whichCommand);
-            String outputOfCommand = IOUtils.toString(whichCommandProcess.getInputStream(), "UTF-8");
-            if (outputOfCommand.contains("/mysql")) {
-                LOGGER.info("mysql is installed [ " + outputOfCommand + "  ] ");
+            int returnCode = ScriptTools.execute(WHICH_MYSQL);
+            if (returnCode == 0) {
                 return true;
             } else {
-                LOGGER.error("mysql is not installed !");
                 return false;
             }
         } catch (Exception exception) {
@@ -97,16 +80,17 @@ public class DatabasePreRequisites implements ServletContextListener {
         }
     }
 
-    public void installMYSQL() {
+    private void installMYSQL() {
         try {
             InputStream installMysqlFileStream = resourceLoader
                     .getResource("classpath:mysql-install-script-template.txt").getInputStream();
-            String installMysqlString = IOUtils.toString(installMysqlFileStream, "UTF-8");
-            String a = StringTools.replaceString(installMysqlString,
-                    Map.of("password", "mib", "mysql-password", "p@ssw0rd"));
-            System.out.println(a);
+            String installMysqlTemplate = IOUtils.toString(installMysqlFileStream, "UTF-8");
+            String installMysqlScript = StringTools.replaceString(installMysqlTemplate,
+                    Map.of("password", sudoPassword, "mysql-password", mysqlPassword));
+            ScriptTools.execute(installMysqlScript);
         } catch (Exception e) {
-
+            LOGGER.error("Something has gone wrong ! ", e);
+            throw new RuntimeException(e);
         }
     }
 
---
+++ commons/src/main/java/self/erp/commons/toolbox/ScriptTools.java
@@ -0,0 +1,38 @@
+package self.erp.commons.toolbox;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.log4j.Logger;
+
+import java.util.Arrays;
+
+public class ScriptTools {
+
+    private static final String BASH_PREFIX = "/bin/bash";
+    private static final String C_FLAG = "-c";
+    private static final Logger LOGGER = Logger.getLogger(ScriptTools.class);
+
+    public static int execute(String command) {
+        Process process;
+        int statusCode;
+        String outputOfCommand;
+        try {
+            String[] whichCommand = new String[] { BASH_PREFIX, C_FLAG, command };
+            LOGGER.info("Starting to execute command [ " + Arrays.toString(whichCommand) + " ]");
+            process = Runtime.getRuntime().exec(whichCommand);
+            statusCode = process.waitFor();
+            if (statusCode == 0) {
+                LOGGER.info("Executed succesfully , return code [ " + statusCode + " ]");
+                outputOfCommand = IOUtils.toString(process.getInputStream(), "UTF-8");
+                LOGGER.info("Output is  [ " + outputOfCommand + " ]");
+            } else {
+                LOGGER.error("Failed to execute , status code [ " + statusCode + " ]");
+                outputOfCommand = IOUtils.toString(process.getErrorStream(), "UTF-8");
+                LOGGER.error("Output is [ " + outputOfCommand + " ]");
+            }
+        } catch (Exception exception) {
+            LOGGER.error("Something has gone wrong !", exception);
+            throw new RuntimeException(exception);
+        }
+        return statusCode;
+    }
+}
